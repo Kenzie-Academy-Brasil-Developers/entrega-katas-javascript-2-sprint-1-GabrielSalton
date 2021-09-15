bf5084cf80c502609be3a2f8e5fd10ca079d1c7f
Gabriel Salton <gabriel.salton.donoso@hotmail.com>
2021-09-15 20:19:04 -0300

    Kata sem bônus finalizado

--- katas2.js
+++ katas2.js
@@ -1,18 +1,32 @@
 // comece a criar a sua função add na linha abaixo
-
-
+function add(a, b) {
+    return a + b;
+}
 // descomente a linha seguinte para testar sua função
 // console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
 
 // comece a criar a sua função multiply na linha abaixo
-
-
+function multiply(c, d){
+    let retorno1 = 0;
+    for (let counter = 1; counter <= d; counter++) {
+        retorno1 = add(retorno1, c)
+    }
+    return retorno1;
+}
 // descomente a linha seguinte para testar sua função
 // console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
 
 // comece a criar a sua função power na linha abaixo
+
+function power(x, n){
+    let powerResult = 1;
+    for (let counter = 1; counter <= n; counter++) {
+        powerResult = multiply(x, powerResult);
+    }
+    return powerResult;
+}
 
 
 // descomente a linha seguinte para testar sua função
@@ -20,6 +34,17 @@
 
 
 // comece a criar a sua função factorial na linha abaixo
+
+function factorial(n) {
+
+    let resultado = 1;
+
+    for(let i = 1; i <= n; i++) {
+        resultado = multiply(resultado, i);
+    }
+    return resultado;
+}
+
 
 
 // descomente a linha seguinte para testar sua função
